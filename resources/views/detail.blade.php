b6e7ea16b91a0f5404f267ca18bc40652bca9497
TrungTinTapCode <trungtintapcode@gmail.com>
2025-07-21 23:43:57 +0700

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -518,6 +518,6 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 
     </div>
-   @include('footer')
+@include('footer')
 </body>
 </html>
